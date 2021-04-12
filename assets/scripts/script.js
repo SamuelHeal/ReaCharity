c92efe0c363f6c80b8bf6c7dc7c37c22b7ccc154
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-04-12 14:05:19 +1000

    create function to generate a div with charity name in a h1, charity website in an anchor tag with a href to the website and charity address in an anchor tag

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,4 +20,45 @@ function filterApiData(stateFilter, causeFilter) {
     return charityData.filter(arrayFilter);
 }
 
+function resultBoxGenerator(filteredData) {
+
+    filteredData.forEach(charity => {
+        //Create container for charity data
+        var containerDiv = document.createElement('div');
+        containerDiv.id = charity._id;
+        
+        //Charity name
+        var nameHeader = document.createElement('h1');
+        var nameText = document.createTextNode(charity.Charity_Legal_Name);
+        nameHeader.appendChild(nameText);
+
+        containerDiv.appendChild(nameHeader);
+
+        //Charity website
+        var websiteAnchor = document.createElement('a');
+        var website = "";
+        if (!charity.Charity_Website.includes("http://") && !charity.Charity_Website.includes("https://")) {
+            website = website +"https://";
+        }
+        website = website + charity.Charity_Website;
+        websiteAnchor.setAttribute("target", "_blank");
+        websiteAnchor.setAttribute("href", website);
+        var webText = document.createTextNode(charity.Charity_Website);
+        websiteAnchor.appendChild(webText);
+        containerDiv.appendChild(websiteAnchor);
+        
+        //Charity address
+        var addressAnchor = document.createElement('a');
+        var appendedAddress = charity.Address_Line_1+", "+charity.Address_Line_2+", "+charity.Address_Line_3+", "+charity.Town_City+ ", " + charity.State+ ", "+ charity.Postcode+ ", "+ charity.Country;
+        var addressText = document.createTextNode(appendedAddress);
+        addressAnchor.appendChild(addressText);
+
+        containerDiv.appendChild(addressAnchor);
+
+        // Attach charity to body
+        var body = document.body;
+        body.insertBefore(containerDiv,body.firstChild)
+    });
+}
+
 queryApiData();
